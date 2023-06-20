808fdb4d48813f532e7f33f7520dae290667d04d
Lucas Ruiz <deth_lucas@hotmail.com>
2023-06-20 13:07:43 -0300

    busca Produtos Paginada

--- dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -5,8 +5,11 @@ import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import com.devsuperior.dscommerce.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController
@@ -18,7 +21,10 @@ public class ProductController {
     @GetMapping(value = "/{id}")
     public ProductDTO findById(@PathVariable Long id) {
        return service.findById(id);
-
     }
 
+    @GetMapping
+    public Page<ProductDTO> findAll(Pageable pageable) {
+    return service.findAll(pageable);
+    }
 }
